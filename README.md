b3230995ac384aa895d595bf48253117225fc2f0
avaneeshweb <97124080+avaneeshweb@users.noreply.github.com>
2026-03-01 16:15:52 -0600

    requirements.txt updated, dockerfile updated, final commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Instructions: 
+1. Run docker compose up
+2. Ctrl-C to stop
+3. Run docker compose down
--- backend/agent.py
+++ backend/agent.py
@@ -20,7 +20,7 @@ def run_agent(req):
     messages = [
         SystemMessage(content = "You are a cooking assistant. Analyze the following message. " \
                                 "Output 1 if it is a general cooking question. " \
-                                "Output 2 if it's a recipe request for a specific dish. " \
+                                "Output 2 if it's a recipe request for a specific dish (ex. how do I cook X?) " \
                                 "Output 3 if it's a 'What can I cook with these ingredients?' query. " \
                                 "Output 0 if it's an irrelevant message. " \
                                 "Do NOT output anything else"),
@@ -28,6 +28,8 @@ def run_agent(req):
     ]
 
     category = llm.invoke(messages).content
+    print("\n\n\n\n\n" + category)
+
     result["in_scope"] = (category != '0')
     if (not result["in_scope"]):
         result["response"] = "Please ask a relevant cooking question"
@@ -52,7 +54,7 @@ def run_agent(req):
     elif (category == '2'):
         messages = [
             SystemMessage(content = f"Keep in mind that the user is limited in cookware and has only the following items: {AVAILABLE_COOKWARE}" \
-                                    "IMPORTANT: if the user cannot cook the recipe with their cookware, output 0 and nothing else"
+                                    "IMPORTANT: if the user CANNOT cook the recipe with their cookware, output 0 and nothing else"
                                     "If you require external resources to validate your answer, output -1 and nothing else." \
                                     "Else, answer the question holistically."
                                     ),
@@ -87,7 +89,9 @@ def run_agent(req):
     if (searched):
         search_results = search(req)
         messages = [
-            SystemMessage(content="You are a cooking assistant. Use these search results to answer the question."),
+            SystemMessage(content="You are a cooking assistant. Use these search results to answer the question. " \
+            "Keep in mind that the user is limited in cookware and has only the following items: {AVAILABLE_COOKWARE}. " \
+            "If they cannot cook the recipe with their cookware, output 'You cannot make this with your cookware.' AND NOTHING ELSE"),
             HumanMessage(content=f"Question: {req}\n\nSearch results: {search_results}")
         ]
 
